7a70330c6d0e9838f38f752e575072978a5a6aef
Patrick <patrick@bluecopperslc.com>
2020-04-14 07:24:35 -0600

    refactored commands, way easier to read now. error msgs are less specific (only one per command) but still effective. throwing a weird unhandled promise rejection, I think it's because I'm deleting the 'msg' object from within the 'message' function but it's not crashing the server so I ain't too worried

--- bot.js
+++ bot.js
@@ -18,18 +18,25 @@ bot.on('message', msg => {
   if (!msg.content) return;
 
   if (msg.author !== bot.user) {
+    let name = msg.member.displayName;
     let cmdAndArgs = msg.content.toLowerCase().split(' ');
     let cmd = cmdAndArgs[0];
     let len = cmdAndArgs.length;
-    if(len === 1) return;
-    let args = cmdAndArgs.slice(1, len); // should it be 2, len?
+    if (len === 1) return;
+    let args = cmdAndArgs.slice(1, len);
     switch (cmd) {
       case 'b': {
-        processBuy(msg, cmd, args);
-        break; // dunno if i need break here or if that's my old java ways lol
+        let min = 20;
+        let max = 900;
+        if (!processBuy(msg, cmd, args, min, max, name))
+          delMsg(msg, `Invalid Message Format. Try: '[b] [price as number, from ${min} to ${max}] [am | pm]`);
+        break;
       }
       case 's': {
-        processSell(msg, cmd, args);
+        let min = 90;
+        let max = 115;
+        if (!processSell(msg, cmd, args, min, max, name))
+          delMsg(msg, `Invalid Message Format. Try: '[s] [price as number, from ${min} to ${max}]`);
         break;
       }
       case 't': {
@@ -41,68 +48,30 @@ bot.on('message', msg => {
   }
 });
 
-processBuy = (msg, cmd, args) => {
-  let err = false;
-  let name = msg.member.displayName;
-  let min = 20;
-  let max = 900;
-  if (args.length !== 2) err = true;
-  if (args[0].match(/^-{0,1}\d+$/) && +args[0] >= min && +args[0] <= max) err = true;
-  if (args[1] !== 'am' && args[1] !== 'pm') err = true;
-  if (!err) {
-    let time = args[1] === 'am' ? 'morning' : 'afternoon';
-    msg.channel.send(name.toUpperCase() + "'s " + nookName() + " are buying turnips for  `" + args[0] + " bells` this " + time + ".").then(res => {
-      msg.delete(DEL_TIMEOUT);
-    });
-  } else {
-    delMsg(msg, "Invalid Message Format. Try: '[b] [price as number, from " + min + " to " + max + " ] [am | pm]");
-  }
+processBuy = (msg, cmd, args, min, max, name) => {
+  if (args.length !== 2) return false;
+  if (!numberBetween(args[0], min, max)) return false;
+  if (args[1] !== 'am' && args[1] !== 'pm') return false;
+  let time = args[1] === 'am' ? 'morning' : 'afternoon';
+  msg.channel.send(name.toUpperCase() + "'s " + nookName() + " are buying turnips for  `" + args[0] + " bells` this " + time + ".").then(res => {
+    msg.delete(DEL_TIMEOUT);
+  }).catch();
+  return true;
 }
 
-processSell = (msg, cmd, args) => {
-  let err = false;
-  let name = msg.member.displayName;
-  let min = 90;
-  let max = 115;
-  if (args.length !== 1) err = true;
-  if (args[0].match(/^-{0,1}\d+$/) && +args[0] >= min && +args[0] <= max) err = true;
-  if (!err) {
-    msg.channel.send(name.toUpperCase() + "'s " + hogName() + " is selling turnips for `" + args[0] + " bells`.").then(res => {
-      msg.delete(DEL_TIMEOUT);
-    });
-  } else {
-    delMsg(msg, "Invalid Message Format. Try: '[s] [price as number, from " + min + " to " + max + " ]");
-  }
+processSell = (msg, cmd, args, min, max, name) => {
+  if (args.length !== 1) return false;
+  if (!numberBetween(args[0], min, max)) return false;
+  msg.channel.send(name.toUpperCase() + "'s " + hogName() + " is selling turnips for `" + args[0] + " bells`.").then(res => {
+    msg.delete(DEL_TIMEOUT);
+  }).catch();
+  return true;
 }
-
-// processMessage = (msg, content) => {
-//   let name = msg.member.displayName;
-//   if (content.length === 3) { // buying price msg
-//     if (content[0] === 'b') {
-//       if (content[2] === 'am' || content[2] === 'pm') {
-//         let time = content[2] === 'am' ? 'morning' : 'afternoon';
-//         if (content[1].match(/^-{0,1}\d+$/) && +content[1] >= 20 && +content[1] <= 900) {
-//           msg.channel.send(name.toUpperCase() + "'s " + nookName() + " are buying turnips for  `" + content[1] + " bells` this " + time + ".").then(res => {
-//             msg.delete(DEL_TIMEOUT);
-//           });
-//         } else delMsg(msg, "Second Value must be a numeric value between 20 and 900.");
-//       } else delMsg(msg, "Third value must be [am | pm], only include this third value if you're listing a buy price.");
-//     } else delMsg(msg, "First value must be [b | s], and must include a third value of [am | pm] if listing a buy price.");
-// } else if (content.length === 2) { //selling price msg
-//   if (content[0] === 's') {
-//     if (content[1].match(/^-{0,1}\d+$/) && +content[1] >= 90 && +content[1] <= 115) {
-//       msg.channel.send(name.toUpperCase() + "'s " + hogName() + " is selling turnips for `" + content[1] + " bells`.").then(res => {
-//         msg.delete(DEL_TIMEOUT);
-//       });
-//     } else delMsg(msg, "Second Value must be a numeric value between 90 and 115.");
-//   } else delMsg(msg, "First value must be [b | s], and must include a third value of [am | pm] if listing a buy price.");
-// } else delMsg(msg, "Invalid Message Format. Try: '[b | s] [price as number] [am | pm (omit if listing sale price)]");
-// }
 
 delMsg = (msg, err) => {
   msg.reply(err).then(res => {
-    res.delete(DEL_TIMEOUT); // deletes the bots response
-    msg.delete(DEL_TIMEOUT); // deletes the original method 
+    res.delete(DEL_TIMEOUT-100); // deletes the bots response
+    msg.delete(DEL_TIMEOUT); // deletes the original message
   });
 }
 
@@ -130,4 +99,10 @@ randomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+numberBetween = (num, min, max) => {
+  if (!num.match(/^-{0,1}\d+$/)) return false;
+  if (num < +min || num > +max) return false;
+  return true;
 }
