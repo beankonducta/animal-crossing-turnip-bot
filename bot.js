322f42470bf6ee7adc81a729ed52ebb0441e0bd1
patrick andrews <patrickxandrews@gmail.com>
2020-06-01 19:02:50 -0600

    more robust word list

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const TIME_ALLOWED = 7500000;
 
 var dgTimer = 0;
 
-var dgWords = ['deathgrips', 'death grips', 'grips', 'death', 'thgr', 'th gr', 'd34thgr1ps', 'andy morin', 'zach hill', 'mc ride', 'spirg htaed', 'spirghtaed', 'захваты смерти'];
+var dgWords = ['deathgrips', 'death grips', 'grips', 'death', 'thgr', 'th gr', 'd34thgr1ps', 'andy morin', 'zach hill', 'mc ride', 'spirg htaed', 'spirghtaed', 'захваты смерти', 'القبضات القاتلة'];
 
 bot.login(TOKEN);
 
