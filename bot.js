1d7e1dc7d4ecea6f75d8e50d23b6309fa79290c4
patrick andrews <patrickxandrews@gmail.com>
2020-06-01 18:49:58 -0600

    made smarter

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const TIME_ALLOWED = 7500000;
 
 var dgTimer = 0;
 
-var dgWords = ['deathgrips', 'death grips', 'grips', 'death'];
+var dgWords = ['deathgrips', 'death grips', 'grips', 'death', 'thgr', 'd34thgr1ps', 'andy morin', 'zach hill', 'mc ride'];
 
 bot.login(TOKEN);
 
@@ -25,19 +25,16 @@ bot.on('message', async msg => {
     // if (msg.channel.name !== channelName) return; // locks bot to specific channel.
     if (!msg.content) return;
     if (msg.author !== bot.user) {
-        let splitString = msg.content.toLowerCase().split(' ');
         for (let word of dgWords) {
-            for (let str of splitString) {
-                if (str.toLowerCase() === word.toLowerCase()) {
-                    let time = msg.createdTimestamp;
-                    if (+time < +dgTimer) {
-                        let dgOffset = (+dgTimer - +time) / 1000;
-                        delMsg(msg, `Can't type anything about death grips for another ${Math.round(dgOffset)} seconds!`)
-                    } else {
-                        dgTimer = time + TIME_ALLOWED;
-                    }
-                    return;
+            if (msg.content.toLowerCase().includes(word.toLowerCase())) {
+                let time = msg.createdTimestamp;
+                if (+time < +dgTimer) {
+                    let dgOffset = (+dgTimer - +time) / 1000;
+                    delMsg(msg, `Can't type anything about death grips for another ${Math.round(dgOffset)} seconds!`)
+                } else {
+                    dgTimer = time + TIME_ALLOWED;
                 }
+                return;
             }
         }
     }
